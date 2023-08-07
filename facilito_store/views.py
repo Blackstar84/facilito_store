8a11f20f4071c25bddf447f81d06d3a821433246
Blackstar84 <carlosalmada84@gmail.com>
2023-08-07 15:52:54 -0400

    se agrega hasta la clase de CICLOS y CONDICIONALES

--- facilito_store/views.py
+++ facilito_store/views.py
@@ -5,9 +5,11 @@ from django.shortcuts import render
 def index(request):
     # return HttpResponse('Hola mundo!')
     return render(request, 'index.html' , {
-        'message': 'Nuevo mensaje desde la vista',
-        'suma': 10 + 20,
-        'booleano': True,
-        'lista': [1,2,3,4],
-        'title': 'Título'
+        'message': 'Listado de productos',        
+        'title': 'Productos',
+        'products': [
+            {'title' : 'Playera', 'price': 5, 'stock': True}, # producto
+            {'title' : 'Camisa', 'price': 7, 'stock': True},
+            {'title' : 'Mochila', 'price': 20, 'stock': False},
+        ]
     })
