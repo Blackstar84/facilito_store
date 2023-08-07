7d1f92f2ba188e0d9018389f3a7788967f077377
Blackstar84 <carlosalmada84@gmail.com>
2023-08-07 15:16:56 -0400

    se agrega hasta la clase Contexto en Django

--- facilito_store/views.py
+++ facilito_store/views.py
@@ -5,5 +5,9 @@ from django.shortcuts import render
 def index(request):
     # return HttpResponse('Hola mundo!')
     return render(request, 'index.html' , {
-        #context
+        'message': 'Nuevo mensaje desde la vista',
+        'suma': 10 + 20,
+        'booleano': True,
+        'lista': [1,2,3,4],
+        'title': 'Título'
     })
