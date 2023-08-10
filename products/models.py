5f9f573c03c2b42eaf0a3ba7b74a715ad192c0b7
Blackstar84 <carlosalmada84@gmail.com>
2023-08-10 16:06:16 -0400

    se agrega hasta la clase de Pre Save

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.db.models.signals import pre_save
 
 # Create your models here.
 
@@ -10,9 +11,17 @@ class Product(models.Model):
     slug = models.SlugField(null=False, blank=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
-    def save(self, *args, **kwgars):
-        self.slug = slugify(self.title)
-        super(Product, self).save(*args, **kwgars)
+    # def save(self, *args, **kwgars):
+    #     self.slug = slugify(self.title)
+    #     super(Product, self).save(*args, **kwgars)
+    
+    
     
     def __str__(self):
         return self.title
+    
+    
+def set_slug(sender, instance,*args, **kwargs): # callback
+    instance.slug = slugify(instance.title)
+    
+pre_save.connect(set_slug, sender=Product)    
