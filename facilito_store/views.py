1cd97b12a6529813c8957c151ed4957420eaeebb
Blackstar84 <carlosalmada84@gmail.com>
2023-08-10 11:04:02 -0400

    se agrega hasta la clase de Listado de Productos

--- facilito_store/views.py
+++ facilito_store/views.py
@@ -4,19 +4,18 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 
+from products.models import Product
+
 from .forms import RegisterForm
 
 
 def index(request):
+    products = Product.objects.all().order_by('-id')
     # return HttpResponse('Hola mundo!')
     return render(request, 'index.html' , {
         'message': 'Listado de productos',        
         'title': 'Productos',
-        'products': [
-            {'title' : 'Playera', 'price': 5, 'stock': True}, # producto
-            {'title' : 'Camisa', 'price': 7, 'stock': True},
-            {'title' : 'Mochila', 'price': 20, 'stock': False},
-        ]
+        'products': products,
     })
     
     
