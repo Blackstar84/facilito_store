eac01c58cbbf93e61a62eaca75ecdfcc36e50f77
Blackstar84 <carlosalmada84@gmail.com>
2023-08-16 11:17:30 -0400

    se agrega hasta la clase Buscador de productos

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from . import views
 from products.views import ProductDetailView
 
 urlpatterns = [
+    path('search',views.ProductSearchListView.as_view(), name='search' ),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product'),
+    
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -28,4 +29,21 @@ class ProductDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         # print(context)
         
+        return context
+    
+class ProductSearchListView(ListView):
+    template_name = 'products/search.html'
+    
+    def get_queryset(self):
+        # Esto equivale a SQL SELECT * FROM products WHERE title like %valor%
+        return Product.objects.filter(title__icontains=self.query())
+    
+    def query(self):
+        return self.request.GET.get('q')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = context['product_list']
+        context['query'] = self.query()
+        context['count'] = context['product_list'].count()
         return context
