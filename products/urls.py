67be56705c8cc9c0965ea62e6e50741ebc91d485
Blackstar84 <carlosalmada84@gmail.com>
2023-08-16 15:06:20 -0400

    se agrega hasta la clase de Rutas con Argumentos

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from products.views import ProductDetailView
 
+app_name = 'products'
+
 urlpatterns = [
     path('search',views.ProductSearchListView.as_view(), name='search' ),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product'),
