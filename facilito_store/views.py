60f8d5fb036dd3776cec2c6ded91c4494bbfbf8c
Blackstar84 <carlosalmada84@gmail.com>
2023-08-17 09:17:05 -0400

    se agrega hasta la clase Usuarios Abstractos

--- facilito_store/views.py
+++ facilito_store/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from users.models import User
 
 from products.models import Product
 
