981ab1377be0a24d3bfe872816e0bea4f2b4011e
Blackstar84 <carlosalmada84@gmail.com>
2023-08-17 13:57:19 -0400

    se agrega hasta la clase de Sesiones

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def cart(request):
+    # Crear una session
+    # request.session['cart_id'] = '123' # Diccionario
+    
+    # Obtenemos el valor de una session
+    valor = request.session.get('cart_id')
+    
+    
+    # Eliminar una session
+    request.session['cart_id'] = None
     
     return render(request, 'carts/cart.html', {})
 
