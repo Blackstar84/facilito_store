cba7c3b4515866ac04a00145304c65a8afcb4357
Blackstar84 <carlosalmada84@gmail.com>
2023-08-17 16:54:37 -0400

    se agrega hasta la clase de Agregar Productos al Carrito

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Cart
 from .utils import get_or_create_cart
+from products.models import Product
 
 
 # Create your views here.
@@ -10,3 +11,14 @@ def cart(request):
     
     return render(request, 'carts/cart.html', {})
 
+
+def add(request):
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+    
+    cart.products.add(product)
+    
+    return render(request, 'carts/add.html',{
+        'product': product
+    })
+    
