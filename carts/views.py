db2a58478a0ca23442544db58e117d6a53e8d462
Blackstar84 <carlosalmada84@gmail.com>
2023-08-17 15:03:44 -0400

    se agrega hasta la clase de Crear Carrito de Compras

--- carts/views.py
+++ carts/views.py
@@ -5,16 +5,17 @@ from .models import Cart
 # Create your views here.
 
 def cart(request):
+    # request.session['cart_id'] = None
     # Con esto obtenemos el usuario autenticado
     user = request.user if request.user.is_authenticated else None
     cart_id = request.session.get('cart_id')
     
     if cart_id:
-        cart = Cart.objects.get(pk=cart_id) # Obtenemos el carrito de la base de datos
+        cart = Cart.objects.get(cart_id=cart_id) # Obtenemos el carrito de la base de datos
     else:
         cart = Cart.objects.create(user=user) # Creamos un nuevo carrito
         
-    request.session['cart_id'] = cart.id    
+    request.session['cart_id'] = cart.cart_id   
     
     return render(request, 'carts/cart.html', {})
 
