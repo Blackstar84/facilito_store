2e6fd2f76395146d91a31a88fb23007f73a3d7dc
Blackstar84 <carlosalmada84@gmail.com>
2023-08-21 14:55:26 -0400

    se agrega hasta la clase de Actualizar Cantidad

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from .models import Cart
+from .models import Cart, CartProducts
 from .utils import get_or_create_cart
 from products.models import Product
 from django.shortcuts import get_object_or_404
@@ -18,11 +18,14 @@ def cart(request):
 def add(request):
     cart = get_or_create_cart(request)
     product = get_object_or_404(Product, pk=request.POST.get('product_id'))
-    quantity = request.POST.get('quantity', 1)
+    quantity = int(request.POST.get('quantity', 1))
     
-    cart.products.add(product, through_defaults={
-        'quantity': quantity
-    })
+    # cart.products.add(product, through_defaults={
+    #     'quantity': quantity
+    # })
+    
+    cart_product = CartProducts.objects.create_or_update_quantity(cart=cart, product=product, quantity=quantity)
+    
     print(cart.products)
     return render(request, 'carts/add.html',{
         'product': product
