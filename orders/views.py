1a01f48e5953856bbcd7358aaf34c7c5ba48fc18
Blackstar84 <carlosalmada84@gmail.com>
2023-08-22 14:00:04 -0400

    se agrega hasta la clase de Resumen del Pedido

--- orders/views.py
+++ orders/views.py
@@ -9,5 +9,6 @@ def order(request):
     cart = get_or_create_cart(request) 
     order = get_or_create_order(cart, request)
     return render(request, 'orders/order.html', {
-        
+       'cart': cart,
+       'order': order,
     })
