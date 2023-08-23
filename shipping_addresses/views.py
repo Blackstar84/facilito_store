96c77acb6aa1c8340a099c38b0f202d839634a6b
Blackstar84 <carlosalmada84@gmail.com>
2023-08-23 11:05:10 -0400

    se agrega hasta la clase de Vista de Direcciones

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ShippingAddress
 
 # Create your views here.
+
+
+class ShippingAddressListView(ListView):
+    model = ShippingAddress
+    template_name = 'shipping_addresses/shipping_addresses.html'
+    
+    def set_queryset(self):
+        return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
