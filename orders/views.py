3c731dfc0080762bfffe06374e10ba5c007cab54
Blackstar84 <carlosalmada84@gmail.com>
2023-08-25 10:03:51 -0400

    se agrega hasta la clase Relación Orden y Dirección

--- orders/views.py
+++ orders/views.py
@@ -21,11 +21,12 @@ def order(request):
 def address(request):
    cart = get_or_create_cart(request)
    order = get_or_create_order(cart, request)
-   shipping_address = order.shipping_address
+   shipping_address = order.get_or_set_shipping_address()
    
    return render(request, 'orders/address.html',{
       'cart': cart,
       'order': order,
+      'shipping_address': shipping_address,
       'breadcrumb': breadcrumb(address=True)
    })
    
