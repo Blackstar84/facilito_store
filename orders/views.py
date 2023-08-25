710ec471f61d534a0d127e4e793d9380578bf7dd
Blackstar84 <carlosalmada84@gmail.com>
2023-08-25 15:20:02 -0400

    se agrega hasta la clase de Seleccionar Dirección

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,9 @@ from .utils import get_or_create_order, breadcrumb
 from carts.utils import get_or_create_cart
 from .models import Order
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
+from shipping_addresses.models import ShippingAddress
 
 
 # Create your views here.
@@ -29,4 +32,30 @@ def address(request):
       'shipping_address': shipping_address,
       'breadcrumb': breadcrumb(address=True)
    })
+   
+   
+@login_required(login_url='login')     
+def select_address(request):
+      shipping_addresses = request.user.shippingaddress_set.all()
+      
+      return render(request, 'orders/select_address.html', {
+         'breadcrumb': breadcrumb(address=True),
+         'shipping_addresses': shipping_addresses
+      })
+   
+@login_required(login_url='login')    
+def check_address(request, pk):
+   cart = get_or_create_cart(request)
+   order = get_or_create_order(cart, request)
+   
+   shipping_address = get_object_or_404(ShippingAddress, pk=pk)
+   
+   if request.user.id != shipping_address.user_id:
+      return redirect('carts:cart')
+   
+   order.update_shipping_address(shipping_address)
+   
+   return redirect('orders:address')
+   
+   
    
