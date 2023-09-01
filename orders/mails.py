1d9c919e12a907a7d907c5b681dba7e3bf44d168
Blackstar84 <carlosalmada84@gmail.com>
2023-09-01 14:44:11 -0400

    se agrega hasta la clase de Links Externos

--- orders/mails.py
+++ orders/mails.py
@@ -2,9 +2,20 @@ from django.template.loader import get_template
 from django.core.mail import EmailMultiAlternatives
 
 from facilito_store import settings
+from django.urls import reverse # Esta función nos retorna un String
 
 class Mail:
     
+    @staticmethod
+    def get_absolute_url(url):
+        if settings.DEBUG: # En modo desarrollo
+            return 'http://localhost:8000{}'.format(
+                reverse(url)
+            )
+    
+    
+    
+       
     @staticmethod
     def send_complete_order(order, user):
         subject = 'Tu pedido ha sido enviado'
@@ -12,6 +23,7 @@ class Mail:
         
         content = template.render({
             'user': user,
+            'next_url': Mail.get_absolute_url('orders:completeds')
             
         })
         
