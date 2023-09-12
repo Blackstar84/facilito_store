5708fa6c38dc3607fc971cf12b06ee20c2e8be8f
Blackstar84 <carlosalmada84@gmail.com>
2023-09-12 13:49:37 -0400

    se agrega hasta la clase de Crear Cliente

--- billing_profiles/views.py
+++ billing_profiles/views.py
@@ -6,6 +6,12 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def create(request):
+    if request.method == 'POST':
+        if request.POST.get('stripeToken'):
+            
+            if not request.user.has_customer():
+                request.user.create_customer_id()
+                
     return render (request,'billing_profiles/create.html', {
         'stripe_public_key': settings.STRIPE_PUBLIC_KEY
     })
