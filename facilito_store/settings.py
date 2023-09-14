3079d2d1f38b059436a74f29f99996eb18208a08
Blackstar84 <carlosalmada84@gmail.com>
2023-09-14 17:00:43 -0400

    se agrega hasta la clase de Paginación

--- facilito_store/settings.py
+++ facilito_store/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'facilito_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'facilito_store',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'facilito_store',
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': ''
+#     }
+# }
 
 
 # Password validation
--- products/views.py
+++ products/views.py
@@ -12,6 +12,7 @@ from products.models import Product
 class ProductListView(ListView): # id -> pk
     template_name = 'index.html'
     queryset = Product.objects.all().order_by('-id')
+    paginate_by = 2
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
